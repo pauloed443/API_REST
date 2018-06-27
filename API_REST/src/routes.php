036cd4bce3150d354a087a279c2086d8cfccaa2e
Unknown <pauloed443@gmail.com>
2018-06-27 18:15:14 -0300

    Criação da rota para adicionar o usuario

--- API_REST/src/routes.php
+++ API_REST/src/routes.php
@@ -10,3 +10,8 @@ $app->get('/', function(Request $request, Response $response, array $args){
 	return $this->renderer->render($response, 'usuarios.phtml', $args);
 });
 
+$app->get('/add-user', function($request, $response, $args){
+    $lista = new Usuario($this->db);
+	$args['lista'] = $lista->getTipoUser();
+    return $this->renderer->render($response, 'cadastro_usuario.phtml', $args);
+});
