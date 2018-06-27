5cf6fba185c2ea92c6b5dc6471ce5c1c498af2fa
Unknown <pauloed443@gmail.com>
2018-06-27 18:14:35 -0300

    criação do metodo para pegar os tipos de usuarios

--- API_REST/src/classes/Usuario.php
+++ API_REST/src/classes/Usuario.php
@@ -10,11 +10,18 @@
 		public function getUsuario(){
 			$array = array();
 
-			$sql = $this->db->query("Select * From usuario");
+			$sql = $this->db->query('Select * From usuario');
 			if ($sql->rowCount() > 0) {
 				$array = $sql->fetchAll();
 			}
 			return $array;
 		}
+        
+        public function getTipoUser(){
+            $array = array();
+            $sql = $this->db->query("select Descricao from tipo_usuario");
+            $array = $sql->fetchAll();
+            return $array;
+        }
 	}
 ?>
