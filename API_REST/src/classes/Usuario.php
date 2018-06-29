a64bbb44e301d4b93be7a6a2ab9174aabdb7fd19
Unknown <pauloed443@gmail.com>
2018-06-28 23:50:55 -0300

    Melhoria e criação de novos metodos

--- API_REST/src/classes/Usuario.php
+++ API_REST/src/classes/Usuario.php
@@ -10,7 +10,28 @@
 		public function getUsuario(){
 			$array = array();
 
-			$sql = $this->db->query('Select * From usuario');
+			$sql = $this->db->query('Select u.Id,u.Nome,u.Email,u.Senha,tu.Descricao 
+									 From usuario u
+									 inner join tipo_usuario tu
+									 on u.Id_Tipo_Usuario = tu.Id');
+			if ($sql->rowCount() > 0) {
+				$array = $sql->fetchAll();
+			}
+			return $array;
+		}
+
+		public function searchUser($user){
+			$array = array();
+			$nome = $user['nome'];
+			$sql = $this->db->query("Select Id,
+											Nome,
+											Email,
+											Senha,
+											Case When Id_Tipo_Usuario = 1 then
+												'Admin'
+											Else 'Comum' end as	Descricao 
+									from usuario 
+									where Nome like '%".$nome."%'");
 			if ($sql->rowCount() > 0) {
 				$array = $sql->fetchAll();
 			}
@@ -19,9 +40,46 @@
         
         public function getTipoUser(){
             $array = array();
-            $sql = $this->db->query("select Descricao from tipo_usuario");
-            $array = $sql->fetchAll();
+            $sql = $this->db->query("select * from tipo_usuario");
+            if ($sql->rowCount() > 0) {
+            	$array = $sql->fetchAll();
+            }
             return $array;
+        }
+
+        public function addUser($lista){
+        	$sql = $this->db->prepare("INSERT INTO usuario
+        								SET Nome = :nome,
+        									Email = :email,
+        									Senha = :senha,
+        									Id_Tipo_Usuario = :tipo_usuario");
+        	$sql->bindValue(":nome", $lista['Nome']);
+        	$sql->bindValue(":email", $lista['Email']);
+        	$sql->bindValue(":senha", $lista['Senha']);
+        	$sql->bindValue(":tipo_usuario", $lista['tipo_usuario']);
+        	$sql->execute();
+        }
+        public function updateUser($lista){
+        	$sql = $this->db->prepare("UPDATE usuario
+        								SET Nome = :nome,
+        									Email = :email,
+        									Senha = :senha
+        								WHERE Email = :email");
+        	$sql->bindValue(":nome", $lista['Nome']);
+        	$sql->bindValue(":email", $lista['Email']);
+        	$sql->bindValue(":senha", $lista['Senha']);
+        	$sql->execute();
+        }
+
+        public function findUserById($id){
+        	$array = array();
+        	$sql = $this->db->prepare("SELECT * FROM usuario WHERE Id = :id");
+        	$sql->bindValue(":id", $id);
+        	$sql->execute();
+        	if ($sql->rowCount() > 0) {
+        		$array = $sql->fetch();
+        	}
+        	return $array;
         }
 	}
 ?>
